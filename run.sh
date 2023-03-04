3bb1a4c2604a058162ce7425e91d15e981de89af
Fethi Gürcan <fethigurcan@gmail.com>
2023-03-04 17:14:25 +0300

    error handling

--- run.sh
+++ run.sh
@@ -1,10 +1,14 @@
-#!/bin/sh
+#!/bin/bash
+function finish {
+    RUNID=`cat tty_logs.txt | grep -o "run with ID '[0-9a-f]\+" | grep -o "[0-9a-f]\+"`
+    if [ -z "$RUNID" ]
+    then
+        echo "ERROR: There is no mlflow run id to log tty_logs.txt!"
+    else
+        #echo $RUNID
+        mlflow artifacts log-artifact -r $RUNID -l tty_logs.txt
+    fi
+}
+trap finish EXIT
+
 mlflow run $@ 2>&1 | tee -a tty_logs.txt
-RUNID=`cat tty_logs.txt | grep -o "run with ID '[0-9a-f]\+" | grep -o "[0-9a-f]\+"`
-if [ -z "$RUNID" ]
-then
-      echo "ERROR: There is no mlflow run id to log tty_logs.txt!"
-else
-      #echo $RUNID
-      mlflow artifacts log-artifact -r $RUNID -l tty_logs.txt
-fi
