7263ddcd5c701ee6dfe88bf5bd5ac96b24acab87
Fethi Gürcan <fethigurcan@gmail.com>
2023-03-04 16:56:29 +0300

    tty logs

--- run.sh
+++ run.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
-mlflow run $@ 2>&1 | tee -a logs.txt
+mlflow run $@ 2>&1 | tee -a tty_logs.txt
+RUNID=`cat tty_logs.txt | grep -o "run with ID '[0-9a-f]\+" | grep -o "[0-9a-f]\+"`
+if [ -z "$RUNID" ]
+then
+      echo "ERROR: There is no mlflow run id to log tty_logs.txt!"
+else
+      #echo $RUNID
+      mlflow artifacts log-artifact -r $RUNID -l tty_logs.txt
+fi
