6bf057aac33ad8edee5216b37bddb0400f73cde9
Fethi Gürcan <fethigurcan@gmail.com>
2023-03-12 16:33:04 +0300

    grep first match fix

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 function finish {
-    RUNID=`cat tty_logs.txt | grep -o "run with ID '[0-9a-f]\+" | grep -o "[0-9a-f]\+"`
+    RUNID=`cat tty_logs.txt | grep -o -m 1 "run with ID '[0-9a-f]\+" | grep -o "[0-9a-f]\+"`
     if [ -z "$RUNID" ]
     then
         echo "ERROR: There is no mlflow run id to log tty_logs.txt!"
